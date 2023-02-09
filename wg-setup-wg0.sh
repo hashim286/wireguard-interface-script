efd694f1785d0fa03e6dba760d906a6936caeff8
hashim286 <112742556+hashim286@users.noreply.github.com>
2023-02-09 16:28:55 -0500

    Update wg-setup-wg0.sh

--- wg-setup-wg0.sh
+++ wg-setup-wg0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip link add dev wg0 type wireguard # you can change to wg0 to whatever you want to name the interface
+ip link add dev wg0 type wireguard # you can change wg0 to whatever you want to name the interface
 ip address add dev wg0 10.0.0.1 peer 10.0.0.2 # enter whatever peer IPs you want here
 wg setconf wg0 /etc/wireguard/name_of_config_file
 ip link set up dev wg0
